6a0109581c657d0ff593650190d9e8c8108da2eb
Marinska <Marinska354@users.noreply.github.com>
2018-12-17 14:47:45 +0700

    content

---
+++ pecahan.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	float hasil;
+	printf("nilai p1 adalah 3/4\n");
+	printf("nilai p2 adalah 2/3\n");
+	hasil=(0.75)*(0.67);
+	printf("Hasil kali p1 dan p2 adalah %.2f",hasil);
+}
